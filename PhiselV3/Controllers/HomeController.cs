8ba9c8438bfe6f58c987981aeae60875b68b5c43
crisferlop <crisrivlop@gmail.com>
2016-03-20 23:16:14 -0600

    primer commit

---
+++ PhiselV3/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhiselV3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getProducts() {
+            SqlConnection cn;
+            SqlCommand cmd;
+            SqlDataReader dr;
+            var data = new List<object>();
+            try
+            {
+                cn = new SqlConnection(
+                    "Data Source=GAMER-PC\\UNDERGROUND;"
+                    + "Initial Catalog=test;Integrated Security=True"
+                    );
+                cn.Open();
+                cmd = new SqlCommand("SELECT Nombre,Id FROM PERSONA",cn);
+                dr = cmd.ExecuteReader();
+                while (dr.Read()) {
+                    data.Add(new { Nombre = dr["Nombre"] });
+                }
+                cn.Close();
+            }
+            catch (SqlException sqle) {
+            }
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ PhiselV3/scripts/app.js
@@ -0,0 +1,12 @@
+﻿myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function ($scope, $http) {
+    $http.get('/home/getProducts')
+    .success(function (result) {
+        $scope.users = result;
+        console.log(result);
+    })
+    .error(function (data) {
+        console.log(data);
+    });
+})
